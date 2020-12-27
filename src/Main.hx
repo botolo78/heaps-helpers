6b1d9b290b4c8a605f62b65a17eec14053daa008
botolo78 <ferrari.david@gmail.com>
2020-12-27 21:33:20 +0100

    visual studio debug tasks refactoring

--- src/Main.hx
+++ src/Main.hx
@@ -1,3 +1,5 @@
+import haxe.Json;
+import h2d.Bitmap;
 import hxd.Res;
 import hxd.Window;
 import utils.*;
@@ -19,8 +21,43 @@ class Main extends hxd.App {
 
         var font : Font = hxd.res.DefaultFont.get();
         var tf = new h2d.Text(font);
-        tf.text = "Hello World";
+        tf.text = "Testing";
         s2d.addChild(tf);
+
+        var map : { layers: Array<{ data: Array<Int> }> } = Json.parse(Res.mymap.entry.getText());
+
+        var t = Res.tileset.toTile();
+        var th = 32;
+        var tw = 32;
+
+        var tileset = [
+            for (y in 0...Std.int(t.height/th))
+                for (x in 0 ...Std.int(t.width/tw))
+                    t.sub(x * tw, y * th, tw, th)
+
+        ];
+        
+        var layerNumber = 0;
+
+        for (layer in map.layers) {
+            var i : Int = 0;
+            for (tile in layer.data) {
+                if (tile==0) {
+                    i++;
+                    continue;
+                }
+                else {
+                    var bmp : Bitmap = new Bitmap(tileset[tile - 1]);
+                    bmp.setPosition(Std.int(i % 20) * tw, Std.int(i / 20) * th);
+                    s2d.addChild(bmp);
+                    i++;
+                }
+            }
+
+            layerNumber++;
+        }
+
+
     }
 
     override function update(dt:Float) {
